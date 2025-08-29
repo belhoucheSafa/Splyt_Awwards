c269206ca6b1781334748316cf34305379a62903
Safa Belhouche <belhouchesafa@gmail.com>
2025-08-29 14:38:20 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
+import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap'
+import MessageSection from './sections/MessageSection'
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <HeroSection />
+      <MessageSection />
+      <div className='h-dvh border border-red-500'></div>
+    </main>
+  )
+}
+
+export default App
