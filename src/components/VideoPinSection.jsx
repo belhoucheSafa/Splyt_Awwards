c6f2f830faaec5899a281e494041384156b694a8
Safa Belhouche <belhouchesafa@gmail.com>
2025-09-02 15:32:44 +0100

    latest changes

--- src/components/VideoPinSection.jsx
+++ src/components/VideoPinSection.jsx
@@ -32,7 +32,7 @@ const VideoPinSection = () => {
         style={{
           clipPath: isMobile
             ? "circle(100% at 50% 50%)"
-            : "circle(10% at 50% 50%)",
+            : "circle(6% at 50% 50%)",
         }}
         className="size-full video-box"
       >
